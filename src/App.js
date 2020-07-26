43a42fcfe359496bb775e611d9657174a4e51029
marsha <marshaannon@gmail.com>
2020-07-26 15:22:47 -0400

    added componentDidMount & call to getCurrentUser, also added connect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux'
+
 import './App.css';
+import Login from './components/Login.js'
+import { getCurrentUser } from './actions/currentUser.js'
 
 class App extends React.Component {
   
+  componentDidMount() {
+    this.props.getCurrentUser()
+  }
   render (){
     return (
-      "Hello, I'm Goalify"
+      <Login />
     );
   }
-
 }
   
 
-export default App;
+export default connect(null, { getCurrentUser })(App);
