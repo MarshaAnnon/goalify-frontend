a0e173580066f8f605ce44e2bb2ddf1dbb9a5329
marsha <marshaannon@gmail.com>
2020-07-26 18:16:45 -0400

    took out logic for login and logout and moved it to components/NavBar, added NavBar component and import

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser.js'
+import NavBar from './components/NavBar.js'
+import MainContainer from './components/MainContainer.js'
 import './App.css';
-import Login from './components/Login.js'
-import Logout from './components/Logout.js'
-
 
 class App extends React.Component {
   
   componentDidMount() {
     this.props.getCurrentUser()
   }
+
   render (){
     return (
-      this.props.currentUser ? <Logout /> : <Login />
+      <div className="App">
+        <NavBar />
+        <MainContainer />
+      </div>
+      
     );
   }
 }
   
-  const mapStateToProps = state => {
-    return {
-      currentUser: state.currentUser
-    }
-  }
+  
 
-export default connect(mapStateToProps, { getCurrentUser })(App);
+export default connect(null, { getCurrentUser })(App);
