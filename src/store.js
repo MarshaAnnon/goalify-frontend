ba595d500cb3a3cd6f58a8442b2220ee3de1f61e
marsha <marshaannon@gmail.com>
2020-07-27 21:19:32 -0400

    added  topics, signupForm, newTopicForm

--- src/store.js
+++ src/store.js
@@ -1,18 +1,24 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
-import users from './reducers/users.js'
-import currentUser from './reducers/currentUser.js'
-import loginForm from './reducers/loginForm.js'
+import users from './reducers/users'
+import currentUser from './reducers/currentUser'
+import loginForm from './reducers/loginForm'
+import topics from './reducers/topics'
+import signupForm from './reducers/signupForm'
+import newTopicForm from './reducers/newTopicForm'
 
 
 const reducer = combineReducers({
-  users: users,
+  users,
   currentUser,
-  loginForm
-  })
-  
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  
-  let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+  loginForm,
+  topics,
+  signupForm,
+  newTopicForm
+})
 
-  export default store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
+export default store
