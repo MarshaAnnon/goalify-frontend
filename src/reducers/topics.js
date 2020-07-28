516d04a1b9d006e1b0cb03b7eaeed3a93ae6626b
marsha <marshaannon@gmail.com>
2020-07-28 12:05:55 -0400

    added ADD_TOPIC & CLEAR_TOPIC

--- src/reducers/topics.js
+++ src/reducers/topics.js
@@ -2,6 +2,10 @@ export default (state = [], action) => {
     switch (action.type) {
         case "SET_TOPICS":
             return action.topics
+        case "ADD_TOPIC":
+            return state.concat(action.topic)
+        case "CLEAR_TOPICS":
+            return []
         default:
             return state
     }
