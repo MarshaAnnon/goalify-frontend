0df3683448bc43d1ddb039bb5c4ca0995b0f8a21
marsha <marshaannon@gmail.com>
2020-07-28 11:59:45 -0400

    added clearTopic, addTopic, & createTopic

--- src/actions/Topics.js
+++ src/actions/Topics.js
@@ -6,6 +6,19 @@ export const setTopics = topics => {
     }
 }
 
+export const clearTopics = () => {
+    return {
+        type: "CLEAR_TOPICS",
+    }
+}
+
+export const addTopic = () => {
+    return {
+        type: "ADD_TOPIC",
+        // topic
+    }
+}
+
 //asynchronous action creators
 export const getTopics = () => {
     return dispatch => {
@@ -23,6 +36,33 @@ export const getTopics = () => {
             } else {
                 console.log(resp.data)
                 dispatch(setTopics(resp.data))
+            }
+        })
+        .catch(console.log)
+    }
+}
+
+export const createTopic = topicData => {
+    return dispatch => {
+        const sendableTripData = {
+            name: topicData.name,
+            user_id: topicData.userId
+        }
+        return fetch("http://localhost:3000/api/v1/topics", {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"   
+            },
+            body: JSON.stringify(sendableTripData)
+        })
+        .then(resp => resp.json())
+        .then(resp => {
+            if (resp.error) {
+                alert(resp.error)
+            } else {
+                console.log(resp.data)
+                dispatch(addTopic(resp.data))
             }
         })
         .catch(console.log)
