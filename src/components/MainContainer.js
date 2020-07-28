ca0cd781ca69c8fabaa23b92e0c00f7cf3a289b2
marsha <marshaannon@gmail.com>
2020-07-27 21:11:03 -0400

    moved topics component inside the div

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Topics from './Topics.js'
 
 const MainContainer = () => {
     return(
         <div className="MainContainer">
-            I am the main container
+            <Topics />        
         </div>
     )
 }
