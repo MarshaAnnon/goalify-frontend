b460fcd77459d288b614d592096e5aa74947774d
marsha <marshaannon@gmail.com>
2020-07-28 16:58:30 -0400

    added links

--- src/components/TopicCard.js
+++ src/components/TopicCard.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const TopicCard = ({ topic }) => {
     return (
-        <h3>{ topic.attributes.name }</h3>
+        topic ?
+        <div>
+            <h3>{ topic.attributes.name }</h3>
+            <Link to={`/topics/${topic.id}/edit`}>Edit this Topic</Link>
+        </div>
+        : null
     )
 }
 
