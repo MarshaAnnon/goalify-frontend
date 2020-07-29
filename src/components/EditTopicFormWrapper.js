9bd77dd6eb140bd036d7be94ad8b1a85a1b0e6f2
marsha <marshaannon@gmail.com>
2020-07-28 20:39:01 -0400

    added componentDidUpdate & componentWillUnmount & added props to render

--- src/components/EditTopicFormWrapper.js
+++ src/components/EditTopicFormWrapper.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { updateTopic } from '../actions/topics'
-import { setFormDataForEdit } from '../actions/topicForm'
+import { updateTopic, deleteTopic } from '../actions/topics'
+import { setFormDataForEdit, resetTopicForm } from '../actions/topicForm'
 import TopicForm from './TopicForm';
 
 class EditTopicFormWrapper extends React.Component {
@@ -9,22 +9,35 @@ class EditTopicFormWrapper extends React.Component {
     componentDidMount() {
         this.props.topic && this.props.setFormDataForEdit(this.props.topic)
     }
-    handleSubmit = (event, topicFormData, history, userId) => {
-        const { updateTopic, topic } = this.props 
-        event.preventDefault()
+
+    componentDidUpdate(prevProps) {
+        this.props.topic && !prevProps.topic && this.props.setFormDataForEdit(this.props.topic)
+    }
+
+    componentWillUnmount() {
+        this.props.resetTopicForm()
+    }
+
+    handleSubmit = (topicFormData) => {
+        const { updateTopic, topic, history } = this.props 
+        
         updateTopic({
             ...topicFormData,
-            topicId: topic.id,
-            userId,
+            topicId: topic.id
         }, history)
     }
     render() {
-        const { history, handleSubmit } = this.props
+        const { history, topic, deleteTopic } = this.props
+        const topicId = topic ? topic.id : null
         return (
-            <TopicForm editMode history={history} handleSubmit={handleSubmit} />
+            <>
+                <TopicForm editMode handleSubmit={this.handleSubmit} />
+                <br />
+                <button style={{color: "red" }} onClick={() => deleteTopic(topicId, history)}>Delete this topic</button>
+            </>
         )
     }
 };
 
 
-export default connect(null, { updateTopic, setFormDataForEdit })(EditTopicFormWrapper);
+export default connect(null, { updateTopic, setFormDataForEdit, resetTopicForm, deleteTopic })(EditTopicFormWrapper);
