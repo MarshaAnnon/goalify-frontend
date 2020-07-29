e58156443edd4a4be1b5debc3ff2fec0662e3162
marsha <marshaannon@gmail.com>
2020-07-29 16:38:38 -0400

    added logic to display goals on a topic show page

---
+++ src/components/Goals.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Goals = ({ goals, topicId }) => {
+
+    const goalCards = goals.length > 0 ?
+        goals.map(goal => (
+            <p key={goal.id}>
+                <Link to={`/topics/${topicId}/goals/${goal.id}`}>
+                    {goal.goal_name}
+                </Link>
+            </p>)) 
+        : null
+
+    return goalCards 
+}
+
+export default Goals;   
+
+
+    
+    
