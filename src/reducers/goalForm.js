fbf937916162cb12048353c390fe7bd7ffd8c471
marsha <marshaannon@gmail.com>
2020-07-31 22:13:09 -0400

    removed the console.logs

--- src/reducers/goalForm.js
+++ src/reducers/goalForm.js
@@ -5,19 +5,15 @@ const initialState = {
 }
 
 export default (state=initialState, action) => {
-    console.log("the incoming action is", action)
-    debugger
     switch (action.type) {
         case "UPDATE_GOAL_FORM":
             const returnVal = {
             ...state,
             [action.formData.name]: action.formData.value
         }
-        console.log("returnVal in the reducer", returnVal)
             return returnVal
 
         case "RESET_GOAL_FORM":
-            debugger
             return initialState
     default:
         return state
