18f5cee69526c7d2338c29558c55675404e12edb
marsha <marshaannon@gmail.com>
2020-08-01 17:16:54 -0400

    removed logout

--- src/components/auth/Logout.js
+++ src/components/auth/Logout.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { logout } from '../../actions/currentUser.js'
-import { withRouter } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 
 const Logout = ({ logout, history }) => {
     
     return (
-        <form onSubmit={(event) => {
+        <a onClick={(event) => {
             event.preventDefault()
             logout()
             history.push('/')
             }   
         } >
-            <input type="submit" value="Log Out" />
-        </form>
+        </a>
     )
 }
 
