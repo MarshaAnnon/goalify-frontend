72f382c5af75888a2e5a24df0ebca93d249526df
marsha <marshaannon@gmail.com>
2020-07-31 21:02:50 -0400

    tweaked form

--- src/actions/goalForm.js
+++ src/actions/goalForm.js
@@ -1,7 +1,6 @@
 // synchronous actions
 export const updateGoalForm = (name, value) => {
     const formData = { name, value }
-     console.log("formData in the action creator", formData)
     return(dispatch) => {
         dispatch(
             {type: "UPDATE_GOAL_FORM", 
