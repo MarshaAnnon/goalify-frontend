2a0619ca50dc8814a4bb93a855a245ca3eb3e6ad
marsha <marshaannon@gmail.com>
2020-08-07 23:54:29 -0400

    remove empty line space

--- src/components/topics/TopicCard.js
+++ src/components/topics/TopicCard.js
@@ -3,20 +3,17 @@ import { Link } from 'react-router-dom'
 import Goals from '../goals/Goals'
 import GoalForm from '../goals/GoalForm'
 
-
 const TopicCard = ({ topic }) => {
     return (
         <div>
             {topic ?
             <div>
                 <h3>{ topic.attributes.name }</h3>
-            <Link to={`/topics/${topic.id}/edit`}>Edit this Topic</Link>
-            <Goals goals={topic.attributes.goals} topicId={topic.id} />
-            <GoalForm topicId={topic.id} />
-        </div>
-        : null}
-        
-        
+                <Link to={`/topics/${topic.id}/edit`}>Edit this Topic</Link>
+                <Goals goals={topic.attributes.goals} topicId={topic.id} />
+                <GoalForm topicId={topic.id} />
+            </div>
+        : null}  
         </div>
     )
 }
