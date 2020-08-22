984158117ce5a10153ac99d8d2f6b7e17e332c6c
marsha <marshaannon@gmail.com>
2020-08-21 23:32:41 -0400

    fix navbar with dropdown menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,34 @@
 import React from 'react'
+import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import { connect } from 'react-redux'
 import { logout } from '../actions/currentUser.js'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const NavBar = ( {logout} ) => {
   return (
-    <Navbar className="navbar" expand="lg" variant="light" bg="light">
-      <Container>
-        <Navbar.Brand href="#">Goalify</Navbar.Brand>
-        <NavDropdown title="Menu" id="collapsible-nav-dropdown">
-          <NavDropdown.Item href="/topics">My Topics</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="/topics/new">Create a New Topic</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="/#" onClick={ logout } >Log Out</NavDropdown.Item>
-        </NavDropdown>
+  <Navbar sticky="top" expand="lg" variant="dark" bg="danger">
+    <Container id='navbar-container'>
+      <Navbar.Brand to="/">Goalify</Navbar.Brand>
+        <Nav className="mr-auto">
+          <NavDropdown title="Menu" id="basic-nav-dropdown">
+            <LinkContainer to="/topics">
+              <NavDropdown.Item>My Topics</NavDropdown.Item>
+            </LinkContainer>
+            <LinkContainer to="/topics/new">
+              <NavDropdown.Item>Create a New Topic</NavDropdown.Item>
+            </LinkContainer>
+            <NavDropdown.Divider/>
+            <LinkContainer to="/#" onClick={logout}>
+              <NavDropdown.Item >Log Out</NavDropdown.Item>
+            </LinkContainer>
+          </NavDropdown>
+        </Nav>
       </Container>
-    </Navbar>
-    )
+  </Navbar>
+  )
 }
 
 export default connect(null, { logout }) (NavBar)
